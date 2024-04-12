3d99c505032ef69248f9acda96fbca014efde06d
janssens <janssensgaetan@gmail.com>
2024-04-12 15:55:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+#
+
+Uses php and google place api to load place openning hours as json string.
+Can be set as cron task generating static file for static website
+
+```
+php src/app.php <lang>
+```
---
+++ src/app.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/env.php';
+
+if (!$google_place_id){
+    echo json_encode(['success' => false]);
+    die();
+}
+
+$language = 'en';
+if (isset($argv[1])){
+    $language = $argv[1];
+}
+
+$url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id='.$google_place_id.'&language='.$language.'&fields=opening_hours&key='.$api_key;
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
+curl_setopt($ch, CURLOPT_URL, $url);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response);
+
+$format = 'json';
+if (isset($argv[2]) && $argv[2] == 'html'){
+    $format = 'html';
+}
+
+if ($format === 'json'){// json
+    echo json_encode($data->result->opening_hours);
+}else{//html
+    echo '<ul>';
+    foreach ($data->result->opening_hours->weekday_text as $wt){
+        echo '<li>'.$wt.'</li>';
+    }
+    echo '</ul>';
+}
+die();
